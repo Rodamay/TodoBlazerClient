8a833c52e97081e6515086af09b44d08997d86bf
blazor@outlook.de <blazor@outlook.de>
2021-06-24 21:30:52 +0200

    implemented todo list

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -39,40 +39,12 @@ namespace TodoBlazorClient.Pages
             await client.PostAsync("api/Todoitems", content);
 
             //Reload all todos
-            /*var result = await client.GetAsync("api/Todoitems");
-            if (result.Content != null)
-            {
-                var todos = await result.Content.ReadAsStringAsync();
-                TodoItems = JsonSerializer.Deserialize(todos, typeof(List<TodoItemDTO>)) as List<TodoItemDTO>;
-            }*/
 
             var streamTask = client.GetStreamAsync("api/Todoitems");
             TodoItems = await JsonSerializer.DeserializeAsync<List<TodoItemDTO>>(await streamTask);
         }
 
-        private async Task UpdateTodo(long id, string name, bool isCompleted)
-        {
-            var client = new HttpClient();
-            client.BaseAddress = new Uri("https://localhost:44354/");
-
-            var todo = new TodoItemDTO
-            { 
-                Id = id,
-                Name = name,
-                IsCompleted = isCompleted,
-            };
-            var url = string.Format(CultureInfo.InvariantCulture, "api/Todoitems/{0}", id);
-            var body = JsonSerializer.Serialize(todo);
-            var content = new StringContent(body.ToString(), Encoding.UTF8, "application/json");
-
-            await client.PutAsync(url, content);
-
-            //Reload all todos
-            var streamTask = client.GetStreamAsync("api/Todoitems");
-            TodoItems = await JsonSerializer.DeserializeAsync<List<TodoItemDTO>>(await streamTask);
-        }
-
-        private async Task DeleteTodo(long id)
+        private async Task CompleteTodo(long id)
         {
             var client = new HttpClient();
             client.BaseAddress = new Uri("https://localhost:44354/");
